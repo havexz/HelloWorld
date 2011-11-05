98dc2efdcdd8d3c65b95fc8602c676bee43313ac
Paramvir Bali <bali.paramvir@gmail.com>
2011-11-04 21:37:05 -0500

    Renamed App.java to HelloWorldMain.java

---
+++ HelloWorld/src/main/java/com/rokoder/adhoc/helloworld/HelloWorldMain.java
@@ -0,0 +1,13 @@
+package com.rokoder.adhoc.helloworld;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
